dc182884553c1ac6518e0d3fdaf13685d768abfb
Jorge Cano <coronero@gmail.com>
2025-12-08 11:54:04 -0800

    changes to lecture

--- Compe160Lab/Lab14/lab14_q1.cpp
+++ Compe160Lab/Lab14/lab14_q1.cpp
@@ -7,7 +7,7 @@ int main()
     double cTemp = 50;
     double fTemp = 96.7;
 
-    cout << "Temp in C: " << CelsiusToFahrenheit(cTemp) << endl;
+    cout << "Temp in F: " << CelsiusToFahrenheit(cTemp) << endl;
 
     cout << "Temp in C: " << FahrenheitToCelsius(fTemp) << endl;
     return 0;
