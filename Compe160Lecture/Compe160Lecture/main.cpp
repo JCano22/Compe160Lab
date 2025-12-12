475bdb6645ee32bdfaa987aebd8a4974c9613b70
Jorge Cano <coronero@gmail.com>
2025-12-11 20:16:28 -0800

    hw lecture

--- Compe160Lecture/Compe160Lecture/main.cpp
+++ Compe160Lecture/Compe160Lecture/main.cpp
@@ -1,30 +1,45 @@
-//
-//  main.cpp
-//  Compe160Lecture
-//
-//  Created by Jorge Cano on 9/3/25.
-//
-
 #include <iostream>
 using namespace std;
 
-struct User{
+void ReversePath(char path[]) {
+    int n;
     
-    int age;
-    string name;
+    for (n = 0; path[n] != '\0'; n++);
     
-};
+    cout << n << endl;
+    
+    for (int j = 0; j < n / 2; j++) {
+        char temp = path[j];
+        path[j] = path[n - j - 1];
+        path[n - j - 1] = temp;
+    }
 
+    int start = 0;
+    while (start < n) {
 
-int main()
-{
-    User user1;
-    
-    cout << "What is your age: ";
-    cin >> user1.age;
-    
-    cout << "What is your name: ";
-    cin >> user1.name;
-    
-    cout << user1.age << ", " << user1.name << endl;
+        int end = start;
+        while (end < n && path[end] != '/') {
+            end++;
+        }
+
+        int left = start;
+        int right = end - 1;
+
+        while (left < right) {
+            char temp = path[left];
+            path[left] = path[right];
+            path[right] = temp;
+            left++;
+            right--;
+        }
+
+        start = end + 1;
+    }
+}
+
+int main() {
+    char somepath[128] = "/usr/local/bin/env/";
+    ReversePath(somepath);
+    cout << somepath << endl;
+    return 0;
 }
